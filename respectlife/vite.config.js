fe3ed978fcc046526fa968068d6c9752c47f5370
cydaddy <lloydpsh@gmail.com>
2025-11-28 15:53:11 +0900

    Add files via upload

---
+++ respectlife/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // IMPORTANT: Change this to '/<REPO_NAME>/' if your repository is not at the root domain.
+})
