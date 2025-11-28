824ad5c7e760bc7af6049b84210bb780060acc4e
cydaddy <lloydpsh@gmail.com>
2025-11-28 16:00:55 +0900

    Update base path in Vite configuration

--- respectlife/vite.config.js
+++ respectlife/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ethics/', // IMPORTANT: Change this to '/ethics/' if your repository is not at the root domain.
+  base: '/ethics/respectlife/', // IMPORTANT: Change this to '/ethics/' if your repository is not at the root domain.
 })
