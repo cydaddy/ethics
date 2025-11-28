388a2a4a2c4380b107fcc88ca93e0152701b2ad7
cydaddy <lloydpsh@gmail.com>
2025-11-28 16:09:11 +0900

    Delete respectlife directory

--- respectlife/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/ethics/', // IMPORTANT: Change this to '/ethics/' if your repository is not at the root domain.
-})
