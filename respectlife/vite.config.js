7dd31bcf72fa25e4b97774efb9706063d36fc4ca
cydaddy <lloydpsh@gmail.com>
2025-11-28 15:59:16 +0900

    Update vite.config.js

--- respectlife/vite.config.js
+++ respectlife/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // IMPORTANT: Change this to '/<REPO_NAME>/' if your repository is not at the root domain.
+  base: './', // IMPORTANT: Change this to '/ethics/' if your repository is not at the root domain.
 })
